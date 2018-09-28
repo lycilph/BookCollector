27fe5e40c87346821dc8ba22d5bc3156c6a80375
Morten Lang <lycilph@gmail.com>
2018-09-28 15:20:06 +0200

    Minor logs screen update

--- BookCollector/Screens/Logs/LogsView.xaml.cs
+++ BookCollector/Screens/Logs/LogsView.xaml.cs
@@ -1,10 +1,17 @@
-﻿namespace BookCollector.Screens.Logs
+﻿using System.Windows;
+
+namespace BookCollector.Screens.Logs
 {
     public partial class LogsView
     {
         public LogsView()
         {
             InitializeComponent();
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            logs_list.ScrollToBottom();
         }
     }
 }
