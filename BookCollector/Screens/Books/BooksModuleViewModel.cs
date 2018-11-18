1b50d47cb8f5b36ec2235dc0d2dc060d070e8075
Morten Lang <lycilph@gmail.com>
2018-11-18 20:21:10 +0100

    Reskinning of books module - partly done

--- BookCollector/Screens/Books/BooksModuleViewModel.cs
+++ BookCollector/Screens/Books/BooksModuleViewModel.cs
@@ -1,20 +1,32 @@
 ﻿using BookCollector.Application;
 using BookCollector.Application.Messages;
 using BookCollector.Screens.Common;
+using Panda.Utils;
 using ReactiveUI;
+using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Windows.Data;
 
 namespace BookCollector.Screens.Books
 {
     public class BooksModuleViewModel : CollectionModuleBase, IBooksModule
     {
         private IStateManager state_manager;
+        private List<BookDetailViewModel> books_view_models;
 
         private SearchFieldViewModel _SearchField;
         public SearchFieldViewModel SearchField
         {
             get { return _SearchField; }
             set { this.RaiseAndSetIfChanged(ref _SearchField, value); }
+        }
+
+        private ICollectionView _Books;
+        public ICollectionView Books
+        {
+            get { return _Books; }
+            set { this.RaiseAndSetIfChanged(ref _Books, value); }
         }
 
         public BooksModuleViewModel(ApplicationNavigationPartViewModel application_navigation_part, 
@@ -31,11 +43,23 @@ namespace BookCollector.Screens.Books
 
         public override void OnActivated()
         {
-            base.OnActivated(); // CollectionModuleBase handles activation or parts
+            base.OnActivated(); // CollectionModuleBase handles activation of parts
 
             // Show message if there are no books in the collection
             if (!state_manager.CurrentCollection.Books.Any())
                 MessageBus.Current.SendMessage(new InformationMessage("No books in collection, import here", "Go", () => MessageBus.Current.SendMessage(NavigationMessage.Import)));
+
+            books_view_models = state_manager.CurrentCollection.Books.Select(b => new BookDetailViewModel(b)).ToList();
+            Books = CollectionViewSource.GetDefaultView(books_view_models);
+        }
+
+        public override void OnDeactivated()
+        {
+            base.OnDeactivated(); // CollectionModuleBase handles deactivation of parts
+
+            Books = null;
+            books_view_models.DisposeAll();
+            books_view_models = null;
         }
     }
 }
